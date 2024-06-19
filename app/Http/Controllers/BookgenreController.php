fb1a0fbe24f3723a3bf5098e556a453d1663f5a7
NaviLeiru <sejismundoi123@gmail.com>
2024-06-18 19:27:05 -0800

    libretto

---
+++ app/Http/Controllers/BookgenreController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\bookgenre;
+use App\Http\Requests\StorebookgenreRequest;
+use App\Http\Requests\UpdatebookgenreRequest;
+
+class BookgenreController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorebookgenreRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(bookgenre $bookgenre)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(bookgenre $bookgenre)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatebookgenreRequest $request, bookgenre $bookgenre)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(bookgenre $bookgenre)
+    {
+        //
+    }
+}
